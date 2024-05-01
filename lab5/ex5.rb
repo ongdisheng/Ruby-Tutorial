5dc29ffe64cd257121042306bf59ffcb919403b7
disheng <ongdisheng@gmail.com>
2024-05-01 12:50:01 +0800

    Added lab 5 solution

---
+++ lab5/ex5.rb
@@ -0,0 +1,22 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+inch_to_cm = 2.54
+pound_to_kg = 0.453592
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+puts "He's #{height * inch_to_cm} cm tall."
+puts "He's #{weight * pound_to_kg} kg heavy."
